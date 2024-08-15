7656d407a814f1b68351267ed1e4f842dd45b7a8
shihoin2 <merysousi@gmail.comm>
2024-08-15 22:46:06 +0900

    error修正(TOPページ)

--- src/components/page/Home/Home.js
+++ src/components/page/Home/Home.js
@@ -53,13 +53,12 @@ const Home = () => {
                                                                 {prefectures.map(
                                                                     prefecture => (
                                                                         <button
+                                                                            key={
+                                                                                prefecture
+                                                                            }
                                                                             type="button"
                                                                             className="py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-                                                                            <li
-                                                                                key={
-                                                                                    prefecture
-                                                                                }
-                                                                                className=" flex items-center justify-center gap-2 m-0">
+                                                                            <li className=" flex items-center justify-center gap-2 m-0">
                                                                                 <Link
                                                                                     href={`/${prefecture}`}>
                                                                                     {
